da3394a863c691edf4829a39baf1ef9c4a5ee194
kiraki-lab <luckykiraki@gmail.com>
2026-06-22 21:12:44 +0900

    Add v2 test override hook

---
+++ challengers/v2-test/test-overrides.js
@@ -0,0 +1,8 @@
+(() => {
+  'use strict';
+
+  if (window.__kirakiV2TestOverridesLoaded) return;
+  window.__kirakiV2TestOverridesLoaded = true;
+
+  // 다음 기능은 공개판을 건드리지 않고 이 파일 또는 v2-test 전용 스크립트에 붙입니다.
+})();
